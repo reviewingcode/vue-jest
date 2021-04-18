05aa4dc9c1bdf2cadd330e7e9cee89b702bdb1c2
Mateen Irshad <reviewingcode@gmail.com>
2021-04-19 00:06:14 +0500

    Add Chat.spec.js

---
+++ src/routes/Chat/Chat.spec.js
@@ -0,0 +1,30 @@
+import Chat from './Chat.vue';
+import { mount, shallowMount } from '@vue/test-utils';
+
+describe('Chat Service', () => {
+
+  let wrapper = null
+  beforeEach(async () => {
+    wrapper = mount(Chat, { propsData: {} });
+  });
+
+  it('should match the snapshot', () => {
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should have a list of items for every message in props', () => {
+
+    const shallowWrapper = shallowMount(Chat, {
+      propsData: {
+        messages: [
+          { content: 'Hello' },
+          { content: 'There!' }
+        ]
+      }
+    });
+
+    expect(shallowWrapper.findAll('.message-display')).toHaveLength(2);
+  });
+
+});
